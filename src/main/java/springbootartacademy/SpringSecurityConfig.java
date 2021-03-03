b3cfac7c87f76d6cfb624c8e001446858efcbace
Art Academy <50003325+ArtAcademy210@users.noreply.github.com>
2021-03-02 21:19:24 -0500

    Update SpringSecurityConfig.java

--- src/main/java/springbootartacademy/SpringSecurityConfig.java
+++ src/main/java/springbootartacademy/SpringSecurityConfig.java
@@ -1,50 +1,47 @@
 package springbootartacademy;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import springbootartacademy.utils.UsuarioDetailsImp;
 
 @Configuration
-@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true)
 public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+ @Autowired
+ private UsuarioDetailsImp usuarioDetailsImp;
+
  @Bean
-	public UserDetailsService userDetailsService() {
-		return new UsuarioDetailsImp();
-	}
- @Bean
- 	public BCryptPasswordEncoder passwordEncoder() {
- 		return new BCryptPasswordEncoder();
- 	}
- 	
- @Bean
- public DaoAuthenticationProvider authenticationProvider() {
-	 DaoAuthenticationProvider daoAuthenticationProvider= new DaoAuthenticationProvider();
-	 daoAuthenticationProvider.setUserDetailsService(userDetailsService());
-	 daoAuthenticationProvider.setPasswordEncoder(passwordEncoder());
-	  return daoAuthenticationProvider;
+ public BCryptPasswordEncoder passwordEncoder() {
+	 return new BCryptPasswordEncoder();
  }
  
- @Override
-	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-		// TODO Auto-generated method stub
-	 auth.authenticationProvider(authenticationProvider());
-	} 
  
+
  @Override
 	protected void configure(HttpSecurity http) throws Exception {
-		// TODO Auto-generated method stub
 		http.authorizeRequests()
-		.antMatchers("/frontend/css/**","/frontend/js/**","/frontend/webfonts/**","/recuperarpassword/**","/resetpassword/**","/formulariocontraseña/**","/cambiarcontraseña/**").permitAll()
-		.anyRequest().authenticated().and()
-		.formLogin().permitAll().and().logout().permitAll();
+		.antMatchers("/frontend/css/**", "/frontend/js/**", "/frontend/webfonts/**", "/images/**").permitAll()
+		.antMatchers("/home").hasAuthority("ADMIN")
+		.anyRequest().authenticated()
+		.and()
+		.formLogin().permitAll()
+		.and()
+		.logout().permitAll();
 	}
- 
+ @Autowired
+	protected void configurerGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(usuarioDetailsImp)
+		.passwordEncoder(passwordEncoder());
+		
+	}
 }
